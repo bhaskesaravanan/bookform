cc99dde4e3aab50fb8436241a3357db21432be82
bhaskesaravanan <bhaskar.saravanan@adaptavantcloud.com>
2017-01-04 13:16:38 +0530

    Add new files

---
+++ models/bookformndbfiles.py
@@ -0,0 +1,20 @@
+from google.appengine.ext import ndb
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+
+class UserDetails(ndb.Model):
+    userName = ndb.StringProperty(required=True)
+    email_ID = ndb.StringProperty(required=True)
+    password = ndb.StringProperty(required = True)
+    uid=ndb.StringProperty(required=True)
+
+class Admins(ndb.Model):
+    username = ndb.StringProperty()
+    email = ndb.StringProperty()
+    password = ndb.StringProperty()
+
+class Books(ndb.Model):
+    name = ndb.StringProperty()
+    genre = ndb.StringProperty()
+    author = ndb.StringProperty()
