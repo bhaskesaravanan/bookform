a2173598051da7b6e8d33f3e3dfd8b77288c6969
surya2706 <karthik.sabapathy@adaptavantcloud.com>
2017-01-04 11:45:19 +0530

    Add my code

--- engineapp/main.py
+++ engineapp/main.py
@@ -1,70 +1,246 @@
-from flask import Flask,url_for,redirect,render_template,request,flash
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from google.appengine.ext import deferred
+from google.appengine.api import mail
+
+import config
+
+# import logging
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
+import uuid, time, datetime
+from functools import wraps
+from ndb_files import *
 
 
-from  google.appengine.ext import ndb
-
-app=Flask(__name__)
-
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 
 
-class UserDetails(ndb.Model):
-    UserName=ndb.StringProperty(required=True)
-    Email_ID=ndb.StringProperty(required=True)
-    Book=ndb.StringProperty(required=True)
-    requestbook=ndb.StringProperty(required=True)
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to login first')
+            return redirect(url_for('loginpopup.html'))
+    return wrap
+
+def userdetails():
+    if request.form['psw'] == request.form['cpsw']:
+        # uname = request.form['uname']
+        pwd = request.form['psw']
+        # hash_pwd = User(pwd)
+        user_details = UserDetails(
+            userName = request.form['uname'],
+            email_ID = request.form['email'],
+            password = generate_password_hash(pwd)
+        )
+        user_details.put()
 
 
-class AddingBooks(ndb.Model):
-    bookname=ndb.StringProperty(required=True)
-    genre=ndb.StringProperty(required=True)
-    Author=ndb.StringProperty(required=True)
+def newbook_request_mailing(to_user, name, book):
+    sender = str('karthik.sabapathy@adaptavantcloud.com')
+    subject_to_user = str("Book Request acknowledgement")
+    mailbody_to_user = str('%s Book has been requested successfully. Thank you for requesting book on Book Forms.' % (book))
+    mail.send_mail(sender, to_user, subject_to_user, mailbody_to_user)
+    to_admin = sender
+    subject_to_admin = str('You have a new Book Request')
+    mailbody_to_admin = str('%s from %s has requested for %s book to be added to the list. Please acknowledge and add the book to the '
+                            'list' % (name, to_user, book))
+    mail.send_mail(sender, to_admin, subject_to_admin, mailbody_to_admin)
 
-
+def readbook_request_mailing():
+    book = request.form['book']
+    sender = str('karthik.sabapathy@adaptavantcloud.com')
+    receiver = str(session['user_email'])
+    subject = str('New Read Book Requested')
+    body = str('Your request to read book %s has been submitted successfully.' % (book))
+    mail.send_mail(sender, receiver, subject, body)
+    receiver = sender
+    subject_to_admin = str('New Read Book Requested')
+    name = str(session['username'])
+    body_to_admin = str('%s from %s has been requested to read %s.' % (name, receiver, book))
+    mail.send_mail(sender, receiver, subject_to_admin, body_to_admin)
 
 @app.route('/')
 def homepage():
+    return render_template('loginpopup.html')
 
-    users = AddingBooks.query().fetch()
-    return render_template('homepage.html', users=users)
-
-
-@app.route('/loginpage')
+@app.route('/loginpage', methods = ['POST'])
 def loginpage():
-     return render_template('AdminLogin.html',)
+    username = request.form['uname']
+    pswd = request.form['psw']
+    if UserDetails.query(UserDetails.email_ID == username).get():
+        user = UserDetails.query(UserDetails.email_ID == username).get()
+        if check_password_hash(user.password, pswd):
+            session['logged_in'] = True
+            session['user_email'] = username
+            session['username'] = user.userName
+            return redirect(url_for('userpage'))
+        else:
+            flash('Invalid username or password.')
+            return redirect(url_for('homepage'))
+    else:
+        flash('Invalid user credentials.')
+        return redirect(url_for('homepage'))
+
+@app.route('/userpage')
+@login_required
+def userpage():
+    books = Books.query().fetch()
+    return render_template('userpage.html',book=books)
+
+@app.route('/userlogout')
+def userlogout():
+    session.pop('user_email', None)
+    session.pop('logged_in', None)
+    session.pop('username', None)
+    return redirect(url_for('homepage'))
+
+@app.route('/bookrequest', methods = ['POST'])
+def bookrequest():
+    book = request.form['requirebook']
+    user_email = session['user_email']
+    name = session['username']
+    newbook_request_mailing(user_email, name, book)
+    flash('Book requested successfully')
+    return redirect(url_for('userpage'))
+
+@app.route('/bookread', methods = ['POST'])
+def bookread():
+    deferred.defer(readbook_request_mailing)
+    flash('Your book will be sent to you shortly.')
+    return redirect(url_for('userpage'))
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    userdet = UserDetails.query().fetch()
+    for user in userdet:
+        if user.email_ID == request.form['email']:
+            flash('The email ID you entered has already been signed up.')
+            return redirect(url_for('homepage'))
+        else:
+            continue
+    else:
+        userdetails()
+        flash('Signed up successfully. Log in to access your account in BookForm.')
+        return redirect(url_for('homepage'))
+
+@app.route('/adminsignup')
+def adminsignup():
+    return render_template('adminsignup.html')
+
+@app.route('/adminrequest')
+def adminrequest():
+    sender = str('karthik.sabapathy@adaptavantcloud.com')
+    to = str(session['user_email'])
+    subject = str('Make me as a Admin')
+    body = str('Click this link and fill up the admin signup form. \nhttps://keepanyname.appspot.com/adminsignup' )
+    mail.send_mail(sender, to, subject, body)
+    flash('Check out your email to get the link for admin form.')
+    return redirect(url_for('userpage'))
+
+@app.route('/signedup', methods=['POST'])
+def signedup():
+    if request.form['adminpassword'] == request.form['confirmpassword']:
+        if UserDetails.query(UserDetails.email_ID == request.form['adminemail']).get():
+            pw = request.form['adminpassword']
+            admins = Admins(
+            username = request.form['adminname'],
+            email = request.form['adminemail'],
+            password = generate_password_hash(pw)
+            )
+            admins.put()
+            flash('Signed up successfully')
+            return redirect(url_for('homepage'))
+        else:
+            flash('You are not a user on BookForms. Only users of BookForms can become Admin on BookForms')
+            return redirect(url_for('adminsignup'))
+    flash('Passwords do not match')
+    return redirect(url_for('adminsignup'))
+
+@app.route('/forgot')
+def forgot():
+    return render_template('forgot_password.html')
+
+@app.route('/forgotpassword', methods=['POST'])
+def forgot_password():
+    uid = uuid.uuid4()
+    now = datetime.now()
+    timestamp = now.time()
+    link = 'https://keepanyname.appspot.com/resetpassword/{}'.format(uid, timestamp)
+    sender = str('karthik.sabapathy@adaptavantcloud.com')
+    to = str(request.form['mailid'])
+    subject = str('Make Password Request - Bookforms')
+    body = str('Click this link to reset your password.\n' + link)
+    mail.send_mail(sender, to, subject, body)
+    # uid = uuid.uuid4()
+    # timestamp = time.time()
+    # link='https://keepanyname.appspot.com/resetpassword/{}?expires_in={}'.format(uid, timestamp + 30*60*1000)
+    # to = str(request.form['mailid'])
+    # sender = str('karthik.sabapathy@adaptavantcloud.com')
+    # subject = str('Link to reset your password')
+    # body = str('Click this link to reset your password' + link)
+    # mail.send_mail(sender, to, subject, body)
+    # flash('Check out your mail to reset the password')
+    # return redirect(url_for('homepage'))
 
 
-@app.route('/login',methods=['POST'])
-def login():
-    if request.form['uname'] == 'admin' and request.form['psw'] == 'admin':
-        return render_template('adminpage.html')
+@app.route('/resetpassword/<uid>/<timestamp>')
+def resetpassword(uid, timestamp):
+    now = datetime.now()
+    now = now.time()
+    if timestamp.hour == now.hour:
+        minutedifference = timestamp.minute - now.minute
+    if minutedifference <= 30:
+        return redirect(url_for(resetpasswords))
+
+    # return 'session expired'
+    # expires_in = request.get("expires_in")
+    # linkcreationtime=datetime.datetime.fromtimestamp(expires_in / 1e3)
+    # currenttime=time.time()
+    # exacttime=datetime.datetime.fromtimestamp(currenttime / 1e3)
+    # timedifference=exacttime-linkcreationtime
+    # if timedifference<=30:
+    #     return render_template('resetpassword.html')
+    #
+    # return 'session expired'
+
+@app.route('/admin', methods=['POST'])
+def admin():
+    adminname = request.form['adminname']
+    adminpsw = request.form['psw']
+    if Admins.query(Admins.email == adminname).get():
+        admin = Admins.query(Admins.email == adminname).get()
+        if check_password_hash(admin.password, adminpsw):
+            session['logged_in'] = True
+            return redirect(url_for('adminpage'))
+        else:
+            flash('Invalid adminname or password.')
+            return redirect(url_for('homepage'))
+    else:
+        flash('Invalid admin credentials.')
+        return redirect(url_for('homepage'))
 
 
-@app.route('/userdetails',methods=['GET'])
-def userdetails():
+@app.route('/adminpage')
+@login_required
+def adminpage():
+    return render_template('adminpage.html')
 
-    user = UserDetails(UserName=request.args.get('name'),Email_ID=request.args.get('mail'),Book=request.args.get('book'),requestbook=request.args.get('requirebook'))
-    user.put()
-    return 'Succesfully registered'
-
-
-
-
-@app.route('/addingbook',methods=['GET'])
+@app.route('/addingbook',methods=['POST'])
 def addingBook():
-    addbook=AddingBooks(bookname=request.args.get('bookname'),genre=request.args.get('genre'),Author=request.args.get('authorname'))
+    addbook=Books(name=request.form['bookname'],genre=request.form['genre'],author=request.form['authorname'])
     addbook.put()
-    # flash('Sucessfully added')
+    flash("Book added successfully")
+    return render_template('adminpage.html')
 
+@app.route('/adminlogout')
+def adminlogout():
+    session.pop('logged_in', None)
+    return redirect(url_for('homepage'))
 
-
-
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
-
-
