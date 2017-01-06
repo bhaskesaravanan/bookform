3b632257ff5131b3ea77e7803a78f91298901e8b
bhaskesaravanan <bhaskar.saravanan@adaptavantcloud.com>
2017-01-06 13:04:39 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-
-if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
-    app_url = 'https://helloworld-151108.appspot.com/'
-else:
-    app_url = 'http://localhost:9080'
